6e15f6812bdd2fc8cf0ae7981f2eb833ec6c22e3
manegame <manusnijhoff@gmail.com>
2018-05-12 20:17:29 +0200

    added mailing list

--- src/views/about.vue
+++ src/views/about.vue
@@ -16,6 +16,19 @@
         </h5>
         <p v-if='main.single_about.acf.text'
            v-html='main.single_about.acf.text' />
+        <form v-if='$route.params.slug === "mailing-list"' action="https://hardemanonline.us15.list-manage.com/subscribe/post?u=cb8eb732cb92c6160af716458&amp;id=ec3eab11ff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+        <div class="mc-field-group">
+          <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+        </div>
+          <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+          </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cb8eb732cb92c6160af716458_ec3eab11ff" tabindex="-1" value=""></div>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </div>
+        </form>
       </div>
     </div>
   </div>
@@ -86,5 +99,12 @@ export default {
       }
     }
   }
+}
+
+input[type='email'] {
+  font-size: $font-size-s;
+  line-height: $line-height-s;
+  padding: 4px 8px;
+  margin-bottom: 10px;
 }
 </style>
